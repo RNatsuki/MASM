815f30551350b273de61cf0100fef1e72f417207
Ibarra <98603993+RNatsuki@users.noreply.github.com>
2024-04-18 07:07:12 -0600

    Update U206.asm

--- files/U2/U206.asm
+++ files/U2/U206.asm
@@ -18,7 +18,10 @@
 
   ; Messages
   msg1 db 'Opcion: '
-
+  capmsg db "Capturando numeros"
+  summsg db "Sumando numeros"
+  resmsg db "Restando numeros"
+  exitmsg db "Saliendo..."
   ;
 
 .CODE
@@ -93,8 +96,6 @@
     LEA bp, msg1 ; Dirección del mensaje
     INT 10h     ; Llamada a la interrupción de video
 
-
-
   tg_m_read:
     MOV ax, 0
     MOV ah, 7
@@ -110,22 +111,67 @@
     mov ah, 2   ; Función para imprimir un carácter
     mov dl, al  ; Carácter a imprimir
     int 21h   ; Llamada a la interrupción de teclado
-
+    MOV opt, al
     ; espera un enter
     MOV ah, 7
     INT 21h
     CMP al, 0Dh
     JNE tg_m_read
 
+    CMP opt, 49
+    JE tg_cap
+    CMP opt, 50
+    JE tg_sum
+    CMP opt, 51
+    JE tg_res
+    CMP opt, 52
+    JE tg_exit
+    JMP tg_menu
 
 
   tg_cap:
-    
+   ; Print message -> Capturando numeros
+    MOV cx, 0
+    MOV ah, 13h ; Función para imprimir un mensaje
+    MOV al, 01h ; Número de líneas a imprimir
+    MOV bh, 00  ; Página de video
+    MOV bl, 0Fh ; Atributo de texto
+    MOV cx, 18  ; Longitud del mensaje
+    MOV dh, 15   ; Fila
+    MOV dl, 29   ; Columna
+    LEA bp, capmsg ; Dirección del mensaje
+    INT 10h     ; Llamada a la interrupción de video
+    JMP tg_exit
+  tg_sum:
+  ; Print message -> Sumando numeros
+    MOV cx, 0
+    MOV ah, 13h ; Función para imprimir un mensaje
+    MOV al, 01h ; Número de líneas a imprimir
+    MOV bh, 00  ; Página de video
+    MOV bl, 0Fh ; Atributo de texto
+    MOV cx, 15  ; Longitud del mensaje
+    MOV dh, 15   ; Fila
+    MOV dl, 29   ; Columna
+    LEA bp, summsg ; Dirección del mensaje
+    INT 10h     ; Llamada a la interrupción de video
+    JMP tg_exit
+  tg_res:
+  ; Print message -> Restando numeros
+    MOV cx, 0
+    MOV ah, 13h ; Función para imprimir un mensaje
+    MOV al, 01h ; Número de líneas a imprimir
+    MOV bh, 00  ; Página de video
+    MOV bl, 0Fh ; Atributo de texto
+    MOV cx, 17  ; Longitud del mensaje
+    MOV dh, 15   ; Fila
+    MOV dl, 29   ; Columna
+    LEA bp, resmsg ; Dirección del mensaje
+    INT 10h     ; Llamada a la interrupción de video
+    JMP tg_exit
 
-
-
-
-
+  tg_exit:
+    MOV ah, 4Ch
+    INT 21h
 
   .EXIT
 
