91a3ef568df18d342095b951437dbc303fabd75b
Ibarra <ibarra2002x3@gmail.com>
2024-05-06 07:28:03 -0600

    Refactor code for improved performance and readability

--- files/U3/U302.asm
+++ files/U3/U302.asm
@@ -5,12 +5,15 @@ ENDIF
 .MODEL small, C
 .STACK
 .DATA
-  txt db 'Hello World!'
+  s1 db 3 dup(' ')
+  base db 100, 10, 1
+  num db 100
 .CODE
   .STARTUP
 
     m_ini ; Initialize the video mode
-    m_imptxt 0fh, 12, 10, 20, txt ; Print the text
+
+    m_impnum num, s1, base, 0fh, 20, 25
 
 
   .EXIT
