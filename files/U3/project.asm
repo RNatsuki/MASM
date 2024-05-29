cd995c3aff9814f5de433dddcc2d0adc9b357488
Ibarra <ibarra2002x3@gmail.com>
2024-05-29 10:34:48 -0600

    Refactor code to include nummen and nummed variables for menu options

--- files/U3/project.asm
+++ files/U3/project.asm
@@ -2,7 +2,9 @@ include macros.inc
 
 .MODEL SMALL, C
 .STACK
+
 .DATA
+  ; Definición de mensajes y variables
   op1 db '1. Capturar numeros'
   op2 db '2. Ver Numeros Capturados'
   op3 db '3. Numero Mayor'
@@ -11,140 +13,161 @@ include macros.inc
   op6 db '6. Salir'
   opmsg db 'Opcion: '
 
-  s1 db 3 dup(' ')
-  base db 100, 10, 1
+  s1 db 3 dup(' ')  ; Buffer para la captura de números
+  base db 100, 10, 1  ; Base para la conversión de números
 
   txt1 db "Numero 1: "
   txt2 db "Numero 2: "
   txt3 db "Numero 3: "
   err db "Numero invalido. Presiona cualquier tecla para continuar..."
 
+  opt db 0  ; La opción del menú seleccionada
 
-  opt db 0 ; La opcion del menu seleccionada
+  txt0 db 6 dup(0)  ; Buffer para texto
 
-  txt0 db 6 dup(0)
+  num1 db 0  ; Primer número capturado
+  num2 db 0  ; Segundo número capturado
+  num3 db 0  ; Tercer número capturado
 
-  num1 db 0
-  num2 db 0
-  num3 db 0
+  nummay db 0  ; Número mayor
+  nummen db 0  ; Número menor
+  nummed db 0  ; Número mediano
 
-  nummay db 0
-  nummen db 0
-  nummed db 0
+.CODE
+  .STARTUP
+    m_ini  ; Inicializa la pantalla
 
-  .CODE
-    .STARTUP
-      m_ini
+    ; Bucle principal del menú
+    e_menu:
+      m_clear  ; Limpia la pantalla
+      m_imptxt 0FH, 19, 2, 30, op1  ; Imprime las opciones del menú
+      m_imptxt 0FH, 25, 3, 30, op2
+      m_imptxt 0FH, 15, 4, 30, op3
+      m_imptxt 0FH, 15, 5, 30, op4
+      m_imptxt 0FH, 25, 6, 30, op5
+      m_imptxt 0FH, 8, 7, 30, op6
 
-      ; MENU
+      ; Captura la opción del menú seleccionada por el usuario
+      m_capnum opt, s1, base, opmsg, 8, err, 9, 35
 
-      e_menu:
-        m_clear
-        m_imptxt 0FH, 19, 2, 30, op1
-        m_imptxt 0FH, 25, 3, 30, op2
-        m_imptxt 0FH, 15, 4, 30, op3
-        m_imptxt 0FH, 15, 5, 30, op4
-        m_imptxt 0FH, 25, 6, 30, op5
-        m_imptxt 0FH, 8, 7, 30, op6
+      ; Compara la opción seleccionada y salta a la etiqueta correspondiente
+      cmp opt, 1
+      je captura
+      cmp opt, 2
+      je ver
+      cmp opt, 3
+      je mayor
+      cmp opt, 4
+      je menor
+      cmp opt, 5
+      je mediana
+      cmp opt, 6
+      je salir
 
-        m_capnum opt, s1, base, opmsg, 8, err, 9, 35
+      ; Si no se selecciona una opción válida, vuelve al menú
+      jmp e_menu
 
+    ; Captura los tres números del usuario
+    captura:
+      m_clear
+      m_capnum num1, s1, base, txt1, 10,  err, 3, 35
+      m_clear
+      m_capnum num2, s1, base, txt2, 10,  err, 3, 35
+      m_clear
+      m_capnum num3, s1, base, txt3, 10, err, 3, 35
+      jmp e_menu
 
-        cmp opt, 1
-        je captura
-        cmp opt, 2
-        je ver
-        cmp opt, 3
-        je mayor
-        cmp opt, 4
-        je menor
-        cmp opt, 5
-        je mediana
-        cmp opt, 6
-        je salir
+    ; Muestra los números capturados
+    ver:
+      m_clear
+      m_impnum num1, s1, base, 0Fh, 15, 30
+      m_impnum num2, s1, base, 0Fh, 16, 30
+      m_impnum num3, s1, base, 0Fh, 17, 30
+      m_waitkey  ; Espera una tecla para continuar
+      jmp e_menu
 
-        jmp e_menu
+    ; Determina y muestra el número mayor
+    mayor:
+      m_clear
+      mov al, num1
+      mov nummay, al
 
-      captura:
-        m_clear
-        m_capnum num1, s1, base, txt1, 10,  err, 3, 35
-        m_clear
-        m_capnum num2, s1, base, txt2, 10,  err, 3, 35
-        m_clear
-        m_capnum num3, s1, base, txt3, 10, err, 3, 35
-        jmp e_menu
-      ver:
-        m_clear
-        m_impnum num1, s1, base, 0Fh, 15, 30
-        m_impnum num2, s1, base, 0Fh, 16, 30
-        m_impnum num3, s1, base, 0Fh, 17, 30
-        m_waitkey
-        jmp e_menu
-      mayor:
-        m_clear
-        mov al, num1
-        mov nummay, al
+      mov al, num2
+      cmp al, nummay
+      jle tg_check3
+      mov nummay, al
 
-        mov al, num2
-        cmp al, nummay
-        jle tg_check3
-        mov nummay, al
+    tg_check3:
+      mov al, num3
+      cmp al, nummay
+      jle tg_display_may
+      mov nummay, al
 
-      tg_check3:
-        mov al, num3
-        cmp al, nummay
-        jle tg_display_may
-        mov nummay, al
+    tg_display_may:
+      m_impnum nummay, s1, base, 0Fh, 15, 30
+      m_waitkey
+      jmp e_menu
 
-      tg_display_may:
-        m_impnum nummay, s1, base, 0Fh, 15, 30
-        m_waitkey
-        jmp e_menu
+    ; Determina y muestra el número menor
+    menor:
+      m_clear
+      mov al, num1
+      mov nummen, al
 
-      menor:
-        m_clear
-        mov al, num1
-        mov nummen, al
+      mov al, num2
+      cmp al, nummen
+      jge tg_check4
+      mov nummen, al
 
-        mov al, num2
-        cmp al, nummen
-        jge tg_check4
-        mov nummen, al
+    tg_check4:
+      mov al, num3
+      cmp al, nummen
+      jge tg_display
 
-      tg_check4:
-        mov al, num3
-        cmp al, nummen
-        jge tg_display
+      mov nummen, al
 
-        mov nummen, al
+    tg_display:
+      m_impnum nummen, s1, base, 0Fh, 15, 30
+      m_waitkey
+      jmp e_menu
 
-      tg_display:
-        m_impnum nummen, s1, base, 0Fh, 15, 30
-        m_waitkey
-        jmp e_menu
-      mediana:
-       m_clear
-        mov al, num1
-        mov nummed, al
+    ; Determina y muestra la mediana de los tres números
+    mediana:
+      m_clear
 
-        mov al, num2
-        cmp al, nummed
-        jge tg_check5
-        mov nummed, al
+      ; Si num1 no es ni el mayor ni el menor, entonces es la mediana
+      mov al, num1
+      cmp al, nummay
+      je tg_find_med_num2
+      cmp al, nummen
+      je tg_find_med_num2
+      mov nummed, al
+      jmp tg_show_mediana
 
-      tg_check5:
-        mov al, num3
-        cmp al, nummed
-        jge tg_display_med
-        mov nummed, al
+    tg_find_med_num2:
+      ; Si num2 no es ni el mayor ni el menor, entonces es la mediana
+      mov al, num2
+      cmp al, nummay
+      je tg_find_med_num3
+      cmp al, nummen
+      je tg_find_med_num3
+      mov nummed, al
+      jmp tg_show_mediana
 
-      tg_display_med:
-        m_impnum nummed, s1, base, 0Fh, 15, 30
-        m_waitkey
-        jmp e_menu
-      salir:
+    tg_find_med_num3:
+      ; Si num3 no es ni el mayor ni el menor, entonces es la mediana
+      mov al, num3
+      mov nummed, al
 
+    tg_show_mediana:
+      m_impnum nummed, s1, base, 0Fh, 15, 30
+      m_waitkey
+      jmp e_menu
 
-    .EXIT
+    salir:
+      ; Sale del programa
+      
+
+.EXIT
 
 END
